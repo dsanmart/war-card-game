4c657339a4317eb8730957ead2d349b4bd6934da
dsanmart <diegosancarrion@gmail.com>
2021-10-16 01:02:50 +0200

    Initial Commit

---
+++ war_card_game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  war_card_game
+//
+//  Created by Diego Sanmartin on 16/10/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ war_card_game/war_card_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_card_gameApp.swift
+//  war_card_game
+//
+//  Created by Diego Sanmartin on 16/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct war_card_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
